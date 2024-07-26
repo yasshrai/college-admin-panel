8139c76ac7c444cc0e9716224b1aaae9b182aade
yash rai <yash2154rai@gmail.com>
2024-07-26 11:05:44 +0530

    fix some color bug in homemessage

--- src/app/components/HomeMessage.tsx
+++ src/app/components/HomeMessage.tsx
@@ -1,7 +1,7 @@
 const HomeMessage = () => {
   return (
     <div className=" bg-gray-950 h-[90vh] md:h-[89vh] w-[98vw] md:w-[85vw] flex flex-col items-center justify-center gap-3">
-      <h1 className=" text-3xl font-bold text-center">
+      <h1 className=" text-3xl font-bold text-center text-white">
         Please! Click on the Sidebar menu for using the feature
       </h1>
     </div>
